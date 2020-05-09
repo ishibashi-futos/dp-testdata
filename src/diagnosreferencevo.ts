9f4c07ff5eb3a036d4c803a53dd372ae34de9391
ishibashi.futos <ishibashi.futos@outlook.com>
2020-05-09 21:25:47 +0900

    SQLを生成するメソッドを生やす

--- src/diagnosreferencevo.ts
+++ src/diagnosreferencevo.ts
@@ -17,12 +17,42 @@ export default interface Diagnosreferencevo extends MonitoringTarget {
   deltaMemory: number
   totalCount: number
   dpUsedMemory: number
+  dpTotalTime: number
   monitoringMode: MonitoringMode
   monitoringCondition: MonitoringCondition
   diffintTime: number
 }
 
-
 export type AgentType = "JavaApplication" | ".NETApplication"
 export type MonitoringMode = "DF"
 export type MonitoringCondition = "DF"
+
+export function toSql(vo: Diagnosreferencevo): string {
+  return "INSERT INTO DIAGNOSREFERENCEVO "
+     + "(TASKID, STARTTIME, ENDTIME, AGENTID, AGENTTYPE, NODEIP, CLIENTIP, TOTALTIME, SYSTEMID, FUNCTIONID, SCREENID, ACTIONID, SESSIONID, USERID,"
+     + "EXCEPTIONTYPE, USEDMEMORY, DELTAMEMORY, TOTALCOUNT, DPTOTALTIME, DPUSEDMEMORY, MONITORINGMODE, MONITORINGCONDITION, DIFFINTIME) VALUES("
+     + `'${vo.taskId}', `
+     + `'${vo.startTime}', `
+     + `'${vo.endTime}', `
+     + `'${vo.agentId}', `
+     + `'${vo.agentType}', `
+     + `'${vo.nodeIp}', `
+     + `'${vo.clientIp}', `
+     + `${vo.totalTime}, `
+     + `'${vo.systemId}', `
+     + `'${vo.functionId}', `
+     + `'${vo.screenId}', `
+     + `'${vo.actionId}', `
+     + `'${vo.sessionId}', `
+     + `'${vo.userId}', `
+     + `'${vo.exceptionType}', `
+     + `${vo.usedMemory}, `
+     + `${vo.deltaMemory}, `
+     + `${vo.totalCount}, `
+     + `${vo.dpTotalTime}, `
+     + `${vo.dpUsedMemory}, `
+     + `'${vo.monitoringMode}', `
+     + `'${vo.monitoringCondition}', `
+     + `${vo.diffintTime}`
+     + ")"
+}
